cf852e4e32fbc141eef9d1eecfbe5d3b684cefe9
justmedusty <dustyn_gibb@hotmail.com>
2023-03-29 13:19:14 -0400

    Initial commit

---
+++ src/main/java/acmecollege/entity/ClubMembership_.java
@@ -0,0 +1,13 @@
+package acmecollege.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2022-08-30T08:53:04.538-0400")
+@StaticMetamodel(ClubMembership.class)
+public class ClubMembership_ extends PojoBase_ {
+	public static volatile SingularAttribute<ClubMembership, StudentClub> club;
+	public static volatile SingularAttribute<ClubMembership, MembershipCard> card;
+	public static volatile SingularAttribute<ClubMembership, DurationAndStatus> durationAndStatus;
+}
---
+++ src/main/java/acmecollege/entity/Student_.java
@@ -0,0 +1,15 @@
+package acmecollege.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SetAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2022-08-30T08:53:04.565-0400")
+@StaticMetamodel(Student.class)
+public class Student_ extends PojoBase_ {
+	public static volatile SingularAttribute<Student, String> firstName;
+	public static volatile SingularAttribute<Student, String> lastName;
+	public static volatile SetAttribute<Student, MembershipCard> membershipCards;
+	public static volatile SetAttribute<Student, CourseRegistration> courseRegistrations;
+}
