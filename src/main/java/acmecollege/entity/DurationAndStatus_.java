be659114c4626cb4ae8254cdc13b10e2dd551cc1
justmedusty <dustyn_gibb@hotmail.com>
2023-03-29 15:14:01 -0400

    i think its done, for realisies now

--- src/main/java/acmecollege/entity/DurationAndStatus_.java
+++ src/main/java/acmecollege/entity/DurationAndStatus_.java
@@ -3,6 +3,7 @@ package acmecollege.entity;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
+import java.time.LocalDateTime;
 
 @Generated(value="Dali", date="2023-03-29T15:11:16.537-0400")
 @StaticMetamodel(DurationAndStatus.class)
--- src/main/java/acmecollege/entity/PojoBase_.java
+++ src/main/java/acmecollege/entity/PojoBase_.java
@@ -3,6 +3,7 @@ package acmecollege.entity;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
+import java.time.LocalDateTime;
 
 @Generated(value="Dali", date="2023-03-29T15:11:16.553-0400")
 @StaticMetamodel(PojoBase.class)
